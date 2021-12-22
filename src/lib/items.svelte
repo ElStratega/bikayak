c8b51aad0fa137272d0e15e8413a1b01ab58f013
ElStratega <lucascatala79@gmail.com>
2021-12-22 05:33:45 +0000

    Develop HTML structure.

---
+++ src/lib/items.svelte
@@ -0,0 +1,9 @@
+<div>
+
+    <form action="" method="">
+        <input type="hidden" name="marcar" aria-label="lolooo" />
+        <button aria-label="Marcar">Sel</button>
+    </form>
+
+
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Cabecera from "$lib/items.svelte";
+    const tit_pag = "bikayaks"
+</script>
+
+<style>
+    .kayax {
+        width: 100%;
+        max-width: 42rem;
+        margin: 4rem auto 0 auto;
+    }
+</style>
+
+<svelte:head>
+    <title>{tit_pag}</title> 
+</svelte:head>
+
+<div class="kayax" style="width:fit-content; height:fit-content; overflow: scroll;">
+
+<h1>{tit_pag}</h1>
+<body>
+<Cabecera />
+</body>
+
+</div>
+   
+
